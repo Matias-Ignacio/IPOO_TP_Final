b247031881bb7aaebf4ff77a2f5471490ace45c6
Matias-Ignacio <matias.riveiro@est.fi.uncoma.edu.ar>
2023-11-29 18:54:54 -0300

    update

--- test/Test.php
+++ test/Test.php
@@ -49,31 +49,31 @@ function menuGeneral(){
                     echo "\n\t************** GESTIONAR EMPRESA **************\n";
                 }  
                 do{
-                    echo "\t\t(1)\tAgregar\n";
-                    echo "\t\t(2)\tBuscar\n";
-                    echo "\t\t(3)\tListar\n";
-                    echo "\t\t(4)\tModificar\n";
-                    echo "\t\t(5)\tEliminar\n\n";
-                    echo "\t\t(0)\tVolver al Menu Principal\n";
+                    echo "\t\t(A)gregar\n";
+                    echo "\t\t(B)uscar\n";
+                    echo "\t\t(L)istar\n";
+                    echo "\t\t(M)odificar\n";
+                    echo "\t\t(E)liminar\n\n";
+                    echo "\t\t(V)olver al Menu Principal\n";
                     echo "\t***********************************************\n";
                     echo "\tIngrese una opción del menú: ";
                     $op2 = trim(fgets(STDIN));
-                }while (!($op2 >= 0 && $op2 < 6));
-                $op2 += $op1;
+                }while (!($op2=="a" && $op2=="b"&& $op2=="l"&& $op2=="m"&& $op2=="e"));
+                $op2 .= $op1;
                 switch ($op2) {
-                    case 11:
+                    case "a10":
                         viajeAgregar();
                         break;
-                    case 12:
+                    case "b10":
                         viajeBuscar();
                         break;
-                    case 13:
+                    case "l10":
                         viajeListar();
                         break;
-                    case 14:
+                    case "m10":
                         viajeModificar();
                         break;  
-                    case 15:
+                    case "e10":
                         viajeEliminar();
                         break;
                     case 21:
